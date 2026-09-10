299c406afbc04ff27f325634671bcc52ca811a46
Paul <5923407+pamls@users.noreply.github.com>
2026-09-10 15:23:14 -0400

    new release

---
+++ assets/NavbarShortcuts-CPJ6YBNk.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./NavbarShortcuts.vue_vue_type_script_setup_true_lang-CaAcerx1.js";import"./index-yEiuA3kB.js";export{m as default};
--- assets/add-customer-CWW1bGvN.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-CjRPBYFy.js";const _=e({__name:"add-customer",setup(t){return(r,n)=>" IN PROGRESS "}});export{_ as default};
