4674a6bdafb4597be71d464996babdabbed9a366
Paul <5923407+pamls@users.noreply.github.com>
2026-09-12 00:19:24 -0400

    new release

--- assets/NavbarShortcuts-DjIVMril.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./NavbarShortcuts.vue_vue_type_script_setup_true_lang-VRHY22jy.js";import"./index-BTSPkKtJ.js";export{m as default};
--- assets/VSpacer-BXugOrDM.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{c as r}from"./createSimpleFunctional-DS2Mj8ae.js";const e=r("v-spacer","div","VSpacer");export{e as V};
